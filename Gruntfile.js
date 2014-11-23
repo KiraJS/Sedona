65a1d49026c0bd9a506caab8f07eef099334e535
isquariel <tavsophi@gmail.com>
2014-11-23 12:00:54 +0300

    Добавить проверку отступов в HTML

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Lint Spaces in code
+        lintspaces: {
+            all: {
+                src: [
+                    '*.html'
+                ],
+                options: {
+                    newline: true,
+                    newlineMaximum: 2,
+                    trailingspaces: true,
+                    indentationGuess: true,
+                    editorconfig: '.editorconfig',
+                    ignores: [
+                        'html-comments',
+                        'js-comments'
+                    ],
+                    showTypes: true,
+                    showCodes: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-lintspaces');
+
+    grunt.registerTask('lint', [ 'lintspaces' ]);
+
+};
